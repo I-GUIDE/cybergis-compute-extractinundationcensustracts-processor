fa0e84ff3592ac4eb0cbf2ff25cd440ce042ee20
Rajesh Kalyanam <rkalyanapurdue@gmail.com>
2022-11-14 11:46:11 -0500

    created new version of the processor based on ellipse construction

--- create_processor_yml.py
+++ create_processor_yml.py
@@ -4,7 +4,8 @@ import yaml
 import os
 
 processor_dict = {'$1': {'ExtractInundationCensusTracts':
-                              {'floodmap_path': '/compute_shared/'+os.environ['param_floodmap_path']}}}
+                              {'floodmap_path': '/compute_shared/'+os.environ['param_floodmap_path'],
+                               'version': os.environ['param_version']}}}
 
 with open('/job/executable/extractinundationcensustracts.yml','w') as procfile:
     yaml.dump(processor_dict,procfile)
