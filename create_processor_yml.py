097dcbfbfd81a484660de26b3b8156fea53f83ad
Rajesh Kalyanam <rkalyanapurdue@gmail.com>
2022-11-17 13:55:55 -0500

    fixed param name

--- create_processor_yml.py
+++ create_processor_yml.py
@@ -3,10 +3,13 @@
 import yaml
 import os
 
+breach_condition = os.environ['param_breach_condition']
+load_condition = os.environ['param_load_condition']
+
 processor_dict = {'$1': {'ExtractInundationCensusTracts':
-                              {'breach_condition': os.environ['breach_condition'],
-                               'load_condition': os.environ['load_condition'],
-                               'floodmap_path': '/compute_shared/Aging_Dams/inundation_maps/NID_FIM_'+os.environ['param_load_condition']+'_'+os.environ['param_breach_condition'],
+                              {'breach_condition': breach_condition,
+                               'load_condition': load_condition,
+                               'floodmap_path': '/compute_shared/Aging_Dams/inundation_maps/NID_FIM_'+load_condition+'_'+breach_condition,
                                'dam_ids': os.environ['param_dam_ids']}}}
 
 with open('/job/executable/extractinundationcensustracts.yml','w') as procfile:
