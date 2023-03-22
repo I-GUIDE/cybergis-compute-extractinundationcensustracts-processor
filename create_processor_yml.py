f682d22ae26ead02a0c5b265c66820a2277aa19c
Rajesh Kalyanam <rkalyana@pal-nat187-10-191.itap.purdue.edu>
2023-03-22 12:13:23 -0400

    Revert "created new version of the processor based on ellipse construction"
    This reverts commit fa0e84ff3592ac4eb0cbf2ff25cd440ce042ee20.

--- create_processor_yml.py
+++ create_processor_yml.py
@@ -4,8 +4,7 @@ import yaml
 import os
 
 processor_dict = {'$1': {'ExtractInundationCensusTracts':
-                              {'floodmap_path': '/compute_shared/'+os.environ['param_floodmap_path'],
-                               'version': os.environ['param_version']}}}
+                              {'floodmap_path': '/compute_shared/'+os.environ['param_floodmap_path']}}}
 
 with open('/job/executable/extractinundationcensustracts.yml','w') as procfile:
     yaml.dump(processor_dict,procfile)
